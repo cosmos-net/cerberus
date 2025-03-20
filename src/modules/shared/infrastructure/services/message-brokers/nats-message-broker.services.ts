db1708d3b3c882038ee89f9ec8130a2552d0e470
2314-RAustin <0315108515@miutt.edu.mx>
2025-03-19 19:07:33 -0700

    feat(message-broker): :sparkles: implement NATS message broker service

---
+++ src/modules/shared/infrastructure/services/message-brokers/nats-message-broker.services.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { ClientProxy, ClientProxyFactory, NatsOptions } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+
+import { IMessageBroker } from '@shared/domain/contracts/message-broker.contract';
+
+@Injectable()
+export class NatsMessageBrokerService implements IMessageBroker {
+  private readonly client: ClientProxy;
+  constructor(options: NatsOptions) {
+    this.client = ClientProxyFactory.create(options);
+  }
+
+  async connect(): Promise<void> {
+    await this.client.connect();
+  }
+
+  async close(): Promise<void> {
+    await this.client.close();
+  }
+
+  getClient(): ClientProxy {
+    return this.client;
+  }
+
+  async status(): Promise<string> {
+    return lastValueFrom(this.client.status);
+  }
+
+  async send<TInput = unknown, TResult = unknown>(pattern: string, data: TInput): Promise<TResult> {
+    return lastValueFrom(this.client.send<TResult, TInput>(pattern, data));
+  }
+
+  async emit<TInput = unknown>(pattern: string, data: TInput): Promise<void> {
+    await lastValueFrom(this.client.emit<TInput>(pattern, data));
+  }
+}
