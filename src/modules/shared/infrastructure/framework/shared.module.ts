cd1122ee0eb67f1e01dedd03def4d01512161fd7
2314-RAustin <0315108515@miutt.edu.mx>
2025-03-22 11:01:56 -0700

    refactor(shared-module): clean up providers by removing unused message broker services

--- src/modules/shared/infrastructure/framework/shared.module.ts
+++ src/modules/shared/infrastructure/framework/shared.module.ts
@@ -3,20 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 
 import { SharedInjectionToken } from '@shared/domain/constants/shared-injection-token.constant';
 import { HadesGatewayService } from '@shared/infrastructure/services/hades-gateway/hades-user-gateway.service';
-import { FactoryMessageBrokerService } from '@shared/infrastructure/services/message-brokers/factory-message-broker.service';
-import { NatsMessageBrokerService } from '@shared/infrastructure/services/message-brokers/nats-message-broker.services';
 
 @Module({
   imports: [ConfigModule],
   controllers: [],
   providers: [
+    HadesGatewayService,
     {
       provide: SharedInjectionToken.HADES_GATEWAY,
       useClass: HadesGatewayService,
     },
-    FactoryMessageBrokerService,
-    NatsMessageBrokerService,
   ],
-  exports: [HadesGatewayService],
+  exports: [SharedInjectionToken.HADES_GATEWAY, HadesGatewayService],
 })
 export class SharedModule {}
