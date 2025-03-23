aaea9843d9b3f5b374c86c941ca2a4bf837ba5c8
2314-RAustin <0315108515@miutt.edu.mx>
2025-03-23 16:53:14 -0700

    feat(core-module): make CoreModule global and import necessary modules

--- src/modules/core/infrastructure/framework/core.module.ts
+++ src/modules/core/infrastructure/framework/core.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 
+import { AuthenticationModule } from '@authentication/infrastructure/framework/authentication.module';
+import { SharedModule } from '@shared/infrastructure/framework/shared.module';
+
+@Global()
 @Module({
-  imports: [],
+  imports: [CqrsModule, SharedModule, AuthenticationModule],
   controllers: [],
   providers: [],
+  exports: [CqrsModule],
 })
 export class CoreModule {}
