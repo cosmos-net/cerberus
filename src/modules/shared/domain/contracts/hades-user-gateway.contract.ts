ed50ac18cf93490f0a06684b0901a88e9316c406
2314-RAustin <0315108515@miutt.edu.mx>
2025-03-26 13:12:55 -0700

    fix(hades-user-gateway): correct import path for IHadesUserCreateResponse and rename interface to IHadesUserGatewayContract

--- src/modules/shared/domain/contracts/hades-user-gateway.contract.ts
+++ src/modules/shared/domain/contracts/hades-user-gateway.contract.ts
@@ -1,6 +1,6 @@
 import { IHadesUserCreateRequest } from '@shared/domain/anti-corruption-layer/hades-user-schemas/create-user/hades-user-create.request';
-import { IHadesUserCreateResponse } from '@shared/domain/anti-corruption-layer/hades-user-schemas/create-user/hades-user.response';
+import { IHadesUserCreateResponse } from '@shared/domain/anti-corruption-layer/hades-user-schemas/create-user/hades-user-create.response';
 
-export interface IHadesGatewayContract {
-  UserWarpOnboard(request: IHadesUserCreateRequest): Promise<IHadesUserCreateResponse>;
+export interface IHadesUserGatewayContract {
+  WarpOnboard(request: IHadesUserCreateRequest): Promise<IHadesUserCreateResponse>;
 }
