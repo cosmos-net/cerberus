d20356978b2834f2fc0b4338bb64e66c0b72ecb8
2314-RAustin <0315108515@miutt.edu.mx>
2025-04-03 21:01:57 -0700

    feat(shared): add JwtAuthGuard, JwtRefreshGuard, JwtStrategy, and JwtRefreshTokenStrategy to SharedModule

--- src/modules/shared/infrastructure/framework/shared.module.ts
+++ src/modules/shared/infrastructure/framework/shared.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { SharedInjectionToken } from '@shared/domain/constants/shared-injection-token.constant';
+import { JwtAuthGuard } from '@shared/infrastructure/guards/jwt-auth.guard';
+import { JwtRefreshGuard } from '@shared/infrastructure/guards/jwt-refresh.guard';
 import { HadesAccountGatewayService } from '@shared/infrastructure/services/hades-gateway/hades-account-gateway.service';
 import { HadesUserGatewayService } from '@shared/infrastructure/services/hades-gateway/hades-user-gateway.service';
+import { JwtRefreshTokenStrategy } from '@shared/infrastructure/strategies/jwt-refresh-token.strategy';
+import { JwtStrategy } from '@shared/infrastructure/strategies/jwt.strategy';
 
 @Module({
   imports: [ConfigModule],
@@ -19,7 +23,18 @@ import { HadesUserGatewayService } from '@shared/infrastructure/services/hades-g
       provide: SharedInjectionToken.HADES_ACCOUNT_GATEWAY,
       useClass: HadesAccountGatewayService,
     },
+    JwtStrategy,
+    JwtRefreshTokenStrategy,
+    JwtAuthGuard,
+    JwtRefreshGuard,
   ],
-  exports: [SharedInjectionToken.HADES_USER_GATEWAY, HadesUserGatewayService],
+  exports: [
+    SharedInjectionToken.HADES_USER_GATEWAY,
+    HadesUserGatewayService,
+    JwtStrategy,
+    JwtRefreshTokenStrategy,
+    JwtAuthGuard,
+    JwtRefreshGuard,
+  ],
 })
 export class SharedModule {}
