37006a4d73b097e871150a62c84485102d095eb7
2314-RAustin <0315108515@miutt.edu.mx>
2025-04-07 21:38:08 -0700

    feat(config): implement JWT configuration in configLoader

--- src/modules/core/infrastructure/framework/loaders/config.loader.ts
+++ src/modules/core/infrastructure/framework/loaders/config.loader.ts
@@ -1,3 +1,10 @@
 import { EnvironmentMapType } from '@core/infrastructure/framework/loaders/environment-map.type';
 
-export const configLoader = (): EnvironmentMapType => ({});
+export const configLoader = (): EnvironmentMapType => ({
+  jwt: {
+    tokenSecret: process.env.JWT_SECRET ?? '',
+    tokenExpiresIn: process.env.JWT_EXPIRATION_TIME ?? '',
+    refreshTokenSecret: process.env.JWT_REFRESH_SECRET ?? '',
+    refreshTokenExpiresIn: process.env.JWT_REFRESH_EXPIRATION_TIME ?? '',
+  },
+});
