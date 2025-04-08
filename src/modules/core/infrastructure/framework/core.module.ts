cc5f0e3e76a686fc0709fa78009f849e4613ccf5
2314-RAustin <0315108515@miutt.edu.mx>
2025-04-07 21:38:38 -0700

    feat(core): enhance CoreModule with JWT configuration and loaders

--- src/modules/core/infrastructure/framework/core.module.ts
+++ src/modules/core/infrastructure/framework/core.module.ts
@@ -1,14 +1,40 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthenticationModule } from '@authentication/infrastructure/framework/authentication.module';
+import { jwtLoader } from '@core/infrastructure/framework/loaders/jwt-loader';
+import { configOption } from '@core/infrastructure/framework/options/config.options';
 import { SharedModule } from '@shared/infrastructure/framework/shared.module';
 
 @Global()
 @Module({
-  imports: [CqrsModule, SharedModule, AuthenticationModule],
+  imports: [
+    ConfigModule.forRoot(configOption),
+    ConfigModule.forFeature(jwtLoader.jwt),
+    CqrsModule,
+    SharedModule,
+    AuthenticationModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule.forFeature(jwtLoader.jwt)],
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get('jwt.tokenSecret'),
+        signOptions: {
+          expiresIn: configService.get('jwt.tokenExpiresIn'),
+        },
+        verifyOptions: {
+          algorithms: ['HS256'],
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
-  exports: [CqrsModule],
+  exports: [CqrsModule, PassportModule, JwtModule],
 })
 export class CoreModule {}
